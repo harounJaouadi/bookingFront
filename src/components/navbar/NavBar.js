077eba76bfec2f4aac4d1b00adcf936c375ebc4e
harounJaouadi <harounlionel@gmail.com>
2023-02-04 00:12:45 +0100

    first Commit

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css";
+
+const NavBar = function () {
+  return (
+    <div className="navbar">
+      <div className="navbarContainer">
+        <span className="logo">imabooking</span>
+        <div className="navItems">
+          <button className="navButton">register</button>
+          <button className="navButton">login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,7 @@
+import NavBar from "../../components/navbar/NavBar";
+
+
+const Home=function(){
+    return <NavBar></NavBar>
+} ; 
+export default Home ; 
